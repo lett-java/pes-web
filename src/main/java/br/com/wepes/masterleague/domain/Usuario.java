76fdd9109b5f9e7653fe21e3b88b6fef236f62c4
Fabio Lettieri <fabiolettieri9@gmail.com>
2020-11-17 19:05:22 -0300

    ADD - Classe usuario e dependencias no pom, Mysql, DevTools e Jpa

---
+++ src/main/java/br/com/wepes/masterleague/domain/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.wepes.masterleague.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	private String nome;
+
+}
