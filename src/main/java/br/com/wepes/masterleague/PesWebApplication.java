a4dffd75470f16cd382de41b5e0ccb2e50d74102
Fabio Lettieri <fabiolettieri9@gmail.com>
2020-11-17 18:38:40 -0300

    Projeto criado

---
+++ src/main/java/br/com/wepes/masterleague/PesWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.wepes.masterleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wepes/masterleague/PesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepes.masterleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
